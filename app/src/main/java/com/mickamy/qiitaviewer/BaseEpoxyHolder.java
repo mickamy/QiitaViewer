45dce60380a75fc2329eb48f1839a6ae3415c164
Tetsuro Mikami <t.mikami1992@gmail.com>
2017-05-21 19:33:45 +0900

    Epoxy の ViewHolder のベースを作成

---
+++ app/src/main/java/com/mickamy/qiitaviewer/BaseEpoxyHolder.java
@@ -0,0 +1,20 @@
+package com.mickamy.qiitaviewer;
+
+import android.support.annotation.CallSuper;
+import android.view.View;
+
+import com.airbnb.epoxy.EpoxyHolder;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseEpoxyHolder extends EpoxyHolder {
+
+    public View itemView;
+
+    @CallSuper
+    @Override
+    protected void bindView(View itemView) {
+        ButterKnife.bind(this, itemView);
+        this.itemView = itemView;
+    }
+}
