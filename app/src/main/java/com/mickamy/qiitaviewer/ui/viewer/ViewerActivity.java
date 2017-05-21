1f5616914ffd13a38ffecac682a86454ddeb21ff
Tetsuro Mikami <t.mikami1992@gmail.com>
2017-05-21 19:59:41 +0900

    ビューワにローディング画面を追加

--- app/src/main/java/com/mickamy/qiitaviewer/ui/viewer/ViewerActivity.java
+++ app/src/main/java/com/mickamy/qiitaviewer/ui/viewer/ViewerActivity.java
@@ -2,11 +2,13 @@ package com.mickamy.qiitaviewer.ui.viewer;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebChromeClient;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.mickamy.qiitaviewer.R;
 
@@ -20,6 +22,9 @@ public class ViewerActivity extends AppCompatActivity {
     @BindView(R.id.webview)
     WebView webView;
 
+    @BindView(R.id.progress)
+    ProgressBar progressBar;
+
     public static Intent createIntent(Context context, String url) {
         Intent intent = new Intent(context, ViewerActivity.class);
         intent.putExtra(KEY_URL, url);
@@ -32,9 +37,25 @@ public class ViewerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_viewer);
         ButterKnife.bind(this);
 
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                enableLoadingView(true);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                enableLoadingView(false);
+            }
+        });
 
         webView.loadUrl(getIntent().getStringExtra(KEY_URL));
+    }
+
+    private void enableLoadingView(boolean loading) {
+        progressBar.setVisibility(loading ? View.VISIBLE : View.GONE);
+        webView.setVisibility(loading ? View.INVISIBLE : View.VISIBLE);
     }
 }
