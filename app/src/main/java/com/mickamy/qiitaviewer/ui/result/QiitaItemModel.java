0f787fd3362f05148fdaa3b306e5617e7e48f568
Tetsuro Mikami <t.mikami1992@gmail.com>
2017-05-21 19:34:08 +0900

    QiitaItem を bind するための Epoxy Model を作成

---
+++ app/src/main/java/com/mickamy/qiitaviewer/ui/result/QiitaItemModel.java
@@ -0,0 +1,46 @@
+package com.mickamy.qiitaviewer.ui.result;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.airbnb.epoxy.EpoxyAttribute;
+import com.airbnb.epoxy.EpoxyModelClass;
+import com.airbnb.epoxy.EpoxyModelWithHolder;
+import com.mickamy.qiitaviewer.BaseEpoxyHolder;
+import com.mickamy.qiitaviewer.R;
+import com.mickamy.qiitaviewer.util.TextFormatUtils;
+
+import org.threeten.bp.OffsetDateTime;
+
+import butterknife.BindView;
+
+import static com.airbnb.epoxy.EpoxyAttribute.Option.DoNotHash;
+import static com.mickamy.qiitaviewer.ui.result.QiitaItemModel.QiitaItemHolder;
+
+@EpoxyModelClass(layout = R.layout.model_qiita_item)
+abstract class QiitaItemModel extends EpoxyModelWithHolder<QiitaItemHolder> {
+
+    @EpoxyAttribute
+    String title;
+
+    @EpoxyAttribute
+    OffsetDateTime createdAt;
+
+    @EpoxyAttribute(DoNotHash)
+    View.OnClickListener onClickListener;
+
+    @Override
+    public void bind(QiitaItemHolder holder) {
+        holder.titleText.setText(title);
+        holder.createdAtText.setText(TextFormatUtils.formatDateTime(createdAt));
+        holder.itemView.setOnClickListener(onClickListener);
+    }
+
+    static class QiitaItemHolder extends BaseEpoxyHolder {
+
+        @BindView(R.id.title_text)
+        TextView titleText;
+        @BindView(R.id.created_at_text)
+        TextView createdAtText;
+    }
+}
